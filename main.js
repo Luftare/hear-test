2ad6c72742ed5c12f01adf50effcabfba3168029
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-07-27 22:23:22 +0300

    Add download button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("graph");
+const canvas = document.querySelector(".graph__canvas");
 const hearTest = new HearTest({
   onTestReady: data => {
     const leftData = data.results.filter(test => test.ear === 0).map(test => test.level);
@@ -8,6 +8,7 @@ const hearTest = new HearTest({
     document.querySelector('.controls__start').disabled = false;
     document.querySelector('.controls__stop').disabled = true;
     document.querySelector('.controls__confirmator').classList.remove('controls__confirmator--red');
+    document.querySelector('.graph__download-button').disabled = false;
   }
 });
 
@@ -24,6 +25,7 @@ document.querySelector('.controls__start').addEventListener('click', () => {
   document.querySelector('.controls__start').disabled = true;
   document.querySelector('.controls__stop').disabled = false;
   document.querySelector('.controls__confirmator').classList.add('controls__confirmator--red');
+  document.querySelector('.graph__download-button').disabled = true;
 });
 
 document.querySelector('.controls__stop').addEventListener('click', () => {
@@ -33,6 +35,12 @@ document.querySelector('.controls__stop').addEventListener('click', () => {
   document.querySelector('.controls__confirmator').classList.remove('controls__confirmator--red');
 });
 
+document.querySelector('.graph__download-button').addEventListener('click', () => {
+  const link = document.getElementById('link');
+  link.setAttribute('download', `hear_test_result_${Date.now()}.png`);
+  link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.click();
+});
 
 window.addEventListener("keydown", (e) => {
   if(e.key === ' ') e.preventDefault();
